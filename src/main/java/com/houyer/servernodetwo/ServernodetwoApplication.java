6677fca823d886931ec6ef487bc79b57aa3a1c95
Chaudement <wuyun_jiaohua@163.com>
2018-07-21 18:36:08 +0800

    Initial commit

---
+++ src/main/java/com/houyer/servernodetwo/ServernodetwoApplication.java
@@ -0,0 +1,14 @@
+package com.houyer.servernodetwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ServernodetwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServernodetwoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/houyer/servernodetwo/ServernodetwoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.houyer.servernodetwo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServernodetwoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
